b57a0fa65b7af99541b9e722b8deb1fd3d6804f2
clementpiat <clement.piat2@gmail.com>
2020-01-20 15:58:44 -0800

    Scraping: big clean up

--- scraping.py
+++ scraping.py
@@ -5,30 +5,31 @@ from datetime import datetime
 import time as t
 import pandas as pd
 
+# Every race within this period of time will be retrieved
 date_start = date(2015,1,1)
-date_end = date(2019,11,26)
+date_end = date(2015,1,3)
 
-def daterange(date1, date2):
-    for n in range(int ((date2 - date1).days)+1):
-        yield date1 + timedelta(n)
- 
-dates = []
-for dt in daterange(date_start, date_end):
-    dates.append(dt.strftime("%d%m%Y"))
+# A simple generator of date being given lower and upper bounds
+def daterange():
+    for n in range(int ((date_end - date_start).days)+1):
+        yield (date_start + timedelta(n)).strftime("%d%m%Y")
 
-# Retrieve programme for each date
+# Retrieve programme for each date and store the responses in the array programmes
+# A programme contains a list of reunions, which contains a list of courses 
 
+programmes = []
 t1 = t.time()
 with FuturesSession() as session:
-    programmes = []
-    urls = ["https://online.turfinfo.api.pmu.fr/rest/client/1/programme/%s?meteo=true&specialisation=INTERNET" % date for date in dates[:3]]
+    urls = ["https://online.turfinfo.api.pmu.fr/rest/client/1/programme/%s?meteo=true&specialisation=INTERNET" % date for date in daterange()]
     future_to_url = {session.get(url): url for url in urls}
     for future in as_completed(future_to_url):
         url = future_to_url[future]
         programmes.append([url, future.result()])
         
-print("Time spent: %s s" % (t.time() - t1))
+print("Time spent for scraping programmes: %s s" % (t.time() - t1))
 
+# Method that dynamically add a new row in the format of an hash to a table
+# If there is a key in the hash which is not present in the table columns, we add a new column
 def add_row(tab, columns, json):
     row = [0]*len(columns)
     for key in json:
@@ -42,48 +43,63 @@ def add_row(tab, columns, json):
             
     return tab, columns
 
+# Create the tables for storing reunions data and courses data
 reunions = []
 reunions_columns = ['date']
 courses = []
 courses_columns = []
+bad_response_programmes = []
+bad_response_columns = ['url', 'status_code']
 
 reunion_id = 0
 course_id = 0
 
-for programme in programmes:
-    programme_json = programme.json()['programme']
-    date = datetime.fromtimestamp(programme_json['date']//1000).strftime("%d%m%Y")
-    print(date)
-    
-    for reunion in programme_json['reunions']:
-        reunion_courses = reunion.pop('courses', None)
-        reunion['reunion_id'] = reunion_id
-        reunion['date'] = date
-        reunions, reunions_columns = add_row(reunions, reunions_columns, reunion)
-        reunion_id += 1
-        for course in reunion_courses:
-            course['reunion_id'] = reunion_id
-            course['course_id'] = course_id
-            courses, courses_columns = add_row(courses, courses_columns, course)
-            course_id += 1
+for url, programme in programmes:
+    # Just in case so that .json() doesn't crash
+    if(programme.status_code == 200):
+        programme_json = programme.json()['programme']
+        date = datetime.fromtimestamp(programme_json['date']//1000).strftime("%d%m%Y")
+        print(date)
+
+        for reunion in programme_json['reunions']:
+            reunion_courses = reunion.pop('courses', None)
+            reunion['reunion_id'] = reunion_id
+            reunion['date'] = date
+            reunions, reunions_columns = add_row(reunions, reunions_columns, reunion)
+            for course in reunion_courses:
+                course['reunion_id'] = reunion_id
+                course['course_id'] = course_id
+                courses, courses_columns = add_row(courses, courses_columns, course)
+                course_id += 1
+            reunion_id += 1
+    else:
+        bad_response_programmes.append([url, programme.status_code])
 
 df_reunions = pd.DataFrame(reunions, columns = reunions_columns)
 df_courses = pd.DataFrame(courses, columns = courses_columns)
+df_bad_response_programmes = pd.DataFrame(bad_response_programmes, columns = bad_response_columns)
 
 df_reunions.to_csv("reunions_%s_%s.csv" % (date_start, date_end))
 df_courses.to_csv("courses%s_%s.csv" % (date_start, date_end))
+df_bad_response_programmes.to_csv("bad_response_programmes%s_%s.csv" % (date_start, date_end))
 
-# Retrieve participans for each date
+# Retrieve participants for each race
 
+# We start by storing the urls corresponding to the api calls needed to get the data
 urls = []
 
 url_to_course_id = {}
+numReunion_index, numOrdre_index = courses_columns.index('numReunion'), courses_columns.index('numOrdre')
+reunion_id_index, course_id_index = courses_columns.index('reunion_id'), courses_columns.index('course_id')
 for course in courses:
-    course_number = "R%s/C%s" % (course['numReunion'], course['numOrdre'])
+    course_number = "R%s/C%s" % (course[numReunion_index], course[numOrdre_index])
+    reunion_id, course_id = course[reunion_id_index], course[course_id_index]
     date = df_reunions.query('reunion_id == %s' % reunion_id).iloc[0]['date']
-    urls.append("https://online.turfinfo.api.pmu.fr/rest/client/1/programme/%s/%s/participants?specialisation=INTERNET" % (date, course_number))
+    url = "https://online.turfinfo.api.pmu.fr/rest/client/1/programme/%s/%s/participants?specialisation=INTERNET" % (date, course_number)
+    urls.append(url)
     url_to_course_id[url] = course_id
 
+# Here we actually do api calls
 t1 = t.time()
 with FuturesSession() as session:
     courses_participants = []
@@ -94,13 +110,26 @@ with FuturesSession() as session:
         
 print("Time spent: %s s" % (t.time() - t1))
 
-participant_id = 0
+# And again we store everything in the table participants
+participants = []
 participants_columns = []
+bad_response_participants = []
+
+participant_id = 0
 
 for url, course_participants in courses_participants:
-    participants = course_participants.json()['participants']
-    for participant in participants:
-        participant['course_id'] = url_to_course_id[url]
-        participant['participant_id'] = participant_id
-        participants, participants_columns = add_row(participants, participants_columns, participant)
-        participant_id += 1
+    if(course_participants.status_code == 200):
+        participants_json = course_participants.json()['participants']
+        for participant in participants_json:
+            participant['course_id'] = url_to_course_id[url]
+            participant['participant_id'] = participant_id
+            participants, participants_columns = add_row(participants, participants_columns, participant)
+            participant_id += 1
+    else:
+        bad_response_participants.append([url, course_participants.status_code])
+
+df_participants = pd.DataFrame(participants, columns = participants_columns)
+df_bad_response_participants = pd.DataFrame(bad_response_participants, columns = bad_response_columns)
+
+df_participants.to_csv("participants_%s_%s.csv" % (date_start, date_end))
+df_bad_response_participants.to_csv("bad_response_participants_%s_%s.csv" % (date_start, date_end))
